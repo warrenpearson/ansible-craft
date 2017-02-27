1c95f5a2a4ab2676ec2bd7e3eb90cddf20bc7329
Warren Pearson <warren.pearson@gmail.com>
2017-02-27 13:42:49 +0000

    Fixes group by issue in MySQL 5.7+. Adds file copy to ansible install script.

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install software-properties-common && sudo apt-add-repository ppa:ansible/ansible && sudo apt-get update && sudo apt-get install ansible
+
+cp -R roles/* /etc/ansible/roles
+cp craft-cms.yml /etc/ansible
+cp hosts /etc/ansible
