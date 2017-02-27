37d231657242fa59865bad0bf7688c06162cbf49
Warren Pearson <warren.pearson@gmail.com>
2017-02-27 11:27:50 +0000

    Fixes execute permissions and a typo.

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install software-properties-common && sudo apt-add-repository ppa:ansible/ansible && sudo apt-get update & & sudo apt-get install ansible
+sudo apt-get install software-properties-common && sudo apt-add-repository ppa:ansible/ansible && sudo apt-get update && sudo apt-get install ansible
